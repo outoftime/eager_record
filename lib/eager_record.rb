5e40c664dcbaa958da3f4220c6dc8196ccf6cf77
Mat Brown <mat@patch.com>
2010-04-26 11:58:50 -0400

    Less janky, but still janky, scoped preloading
    Refactored the scoped preloading code so that there is a single "dummy"
    association that is always used for scoped preloading. Eager record
    manually creates an association collection with the correct scoping
    information and adds it to the records in question before telling AR to
    preload the association (by passing the reflection directly), so we're
    no longer creating a new association for each set of conditions that
    might be passed in. But the code is still very messy and needs cleanup
    -- at least this is a general approach I like.

--- lib/eager_record.rb
+++ lib/eager_record.rb
@@ -16,6 +16,7 @@ module EagerRecord
       (class <<base; self; end).module_eval do
         alias_method_chain :find_by_sql, :eager_preloading
       end
+      base.has_many :_temporary_association_for_scoped_preloading, :class_name => 'Object', :readonly => true
     end
 
     def find_by_sql_with_eager_preloading(*args)
@@ -87,21 +88,39 @@ module EagerRecord
     def find_with_eager_preloading(*args)
       options = args.extract_options!
       options_digest = Digest::SHA1.hexdigest(options.inspect)[0..7]
-      association_name = :"_preloadable_association_collection_#{options_digest}s"
-      if false #FIXME options already exist
-      else
-        @owner.class.has_many(
+      association_name = :"_temporary_association_for_scoped_preloading"
+      reflection_name = @reflection.name
+      current_scope = @reflection.options.merge(current_scoped_methods[:find])
+      scope_key = current_scope.inspect
+      if scoped_preloaded_associations = @owner.instance_variable_get(:@scoped_preloaded_associations)
+        if preloaded_association = scoped_preloaded_associations[reflection_name][scope_key]
+          return preloaded_association
+        end
+      end
+      if originating_collection = @owner.instance_variable_get(:@originating_collection)
+        reflection = @owner.class.__send__(
+          :create_has_many_reflection,
           association_name,
-          @reflection.options.merge(current_scoped_methods[:find]).merge(
+          current_scope.merge(
             :class_name => @reflection.klass.name,
             :readonly => true
           )
         )
+        originating_collection.each do |record|
+          association = ActiveRecord::Associations::HasManyAssociation.new(record, reflection)
+          record.__send__(:association_instance_set, association_name, association)
+        end
+        @owner.class.__send__(:preload_has_many_association, originating_collection, reflection)
+        originating_collection.each do |record|
+          record.instance_eval do
+            @scoped_preloaded_associations ||= Hash.new { |h, k| h[k] = {} }
+            @scoped_preloaded_associations[reflection_name][scope_key] =
+              association_instance_get(association_name)
+            association_instance_set(association_name, nil)
+          end
+        end
       end
-      if originating_collection = @owner.instance_variable_get(:@originating_collection)
-        @owner.class.__send__(:preload_associations, originating_collection, association_name)
-      end
-      @owner.__send__(association_name)
+      @owner.instance_variable_get(:@scoped_preloaded_associations)[reflection_name][scope_key]
     end
   end
 end
